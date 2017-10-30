6ce6850dcaf9f5405a219ce7d4e416a79e953b94
jondashkyle <contact@jon-kyle.com>
2017-10-29 22:21:25 -0700

    build

--- src/components/wrapper-site.js
+++ src/components/wrapper-site.js
@@ -4,9 +4,7 @@ module.exports = wrapper
 
 function wrapper (view) {
   return function (state, emit) {
-    // console.log(state.dropout.loaded)
     if (!state.dropout) return noArchive()
-    // return noArchive()
 
     return html`
       <body class="bgc-white fc-black ff-sans lh1-5">
@@ -21,12 +19,12 @@ function noArchive (state, emit) {
     <body class="bgc-white fc-black ff-sans lh1-5 vhmn100 vw100 x xjc xac p2">
       <div class="markdown-body ff-heading copy fs1-5">
         <p>When interfaces are designed for capturing and exhausting your attention going offline is both an act of liberation and luxury. This is a tool of ethical technology enabling you to save pages for offline access and personal archival.</p>
-        <p>The project requires Beaker Browser and it’s experimental <code>web api</code> to function. Good news; it’s really easy to start, and once downloaded you can begin browsing and publishing to the rest of the p2p web, too.</p>
+        <p>The project requires Beaker Browser and it's experimental <code>web api</code> to function. Good news; it's really easy to start, and once downloaded you can begin browsing and publishing to the rest of the p2p web, too.</p>
         <ol>
           <li>Download and install <a href="http://beakerbrowser.com">Beaker Browser</a>.</li>
           <li>Navigate to <a href="dat://dropout.jon-kyle.com/">the Dat archive url</a>.</li>
-          <li>If you’d like to create your own, click Fork inside Beaker. Feel free to customize as the entire source is included.</li>
-        </ul>
+          <li>If you'd like to create your own, click Fork inside Beaker. Feel free to customize as the entire source is included.</li>
+        </ol>
         <p>For more, visit the <a href="https://github.com/jondashkyle/dropout-beaker">repository</a> or <a href="https://jon-kyle.com/entries/2017-10-26-dropout/">the log entry</a>.
       </div>
     </body>
